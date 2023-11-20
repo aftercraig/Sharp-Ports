227d7aadedd362b247a1a50223c7eaa19c4c39ea
aftercraig <pavelboritko@gmail.com>
2023-11-20 21:05:56 +0500

    minor update

--- Program.cs
+++ Program.cs
@@ -7,32 +7,32 @@ using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, 
 //var tcpListener = new TcpListener(IPAddress.Any, 8888);
 try
 {
-// Подключение к серверу
-await tcpClient.ConnectAsync("192.168.1.104", 1777);
-while (true)
-{
+    // Подключение к серверу
+    await tcpClient.ConnectAsync("192.168.1.104", 1777);
+    while (true)
+    {
 
-// чтение изображения ввиде массива
-byte[] imageData = File.ReadAllBytes("img/123.jpg");
+    // чтение изображения ввиде массива
+    byte[] imageData = File.ReadAllBytes("img/123.jpg");
 
-//Создание сообщения
-//System.Console.WriteLine("Введите команду для сервера");
-//string command = Console.ReadLine() + '\n';
+    //Создание сообщения
+    //System.Console.WriteLine("Введите команду для сервера");
+    //string command = Console.ReadLine() + '\n';
 
-//Первеод запроса из строкового типа данных в массив байт
-byte[] requestData = File.ReadAllBytes("img/123.jpg");
+    //Первеод запроса из строкового типа данных в массив байт
+    byte[] requestData = File.ReadAllBytes("img/123.jpg");
 
-// Отправка сообщения серверу
-await tcpClient.SendAsync(requestData, SocketFlags.None);
+    // Отправка сообщения серверу
+    await tcpClient.SendAsync(requestData, SocketFlags.None);
 
-// Оповещение о состоянии сообщения
-Console.WriteLine("Изображение отправленно");
+    // Оповещение о состоянии сообщения
+    Console.WriteLine("Изображение отправленно");
 
-// Сообщение буфера для приема ответа от сервера
-byte[] data = new byte[512];
-}
+    // Сообщение буфера для приема ответа от сервера
+    byte[] data = new byte[512];
+    }
 }
 catch (Exception ex)
 {
-Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.Message);
 }
