dbd016714f7c55cc07c926b5474f77b9451e16c5
aftercraig <pavelboritko@gmail.com>
2023-12-06 03:06:22 +0500

    major changes

--- Program.cs
+++ Program.cs
@@ -3,36 +3,22 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 
-using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-//var tcpListener = new TcpListener(IPAddress.Any, 8888);
-try
-{
-    // Подключение к серверу
-    await tcpClient.ConnectAsync("192.168.1.104", 1777);
-    while (true)
+using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); 
+try {
+    await tcpClient.ConnectAsync("192.168.220.139", 1777);
+    while(true)
     {
 
-    // чтение изображения ввиде массива
-    byte[] imageData = File.ReadAllBytes("img/123.jpg");
+        string path = ".img/qwe.jpg";
 
-    //Создание сообщения
-    //System.Console.WriteLine("Введите команду для сервера");
-    //string command = Console.ReadLine() + '\n';
+        byte[] reqestData = File.ReadAllBytes(path);
 
-    //Первеод запроса из строкового типа данных в массив байт
-    byte[] requestData = File.ReadAllBytes("img/123.jpg");
+        await tcpClient.SendAsync(reqestData);
 
-    // Отправка сообщения серверу
-    await tcpClient.SendAsync(requestData, SocketFlags.None);
-
-    // Оповещение о состоянии сообщения
-    Console.WriteLine("Изображение отправленно");
-
-    // Сообщение буфера для приема ответа от сервера
-    byte[] data = new byte[512];
+        Console.WriteLine("Изображение отправленно!");
+        
     }
 }
-catch (Exception ex)
-{
+catch (Exception ex) {
     Console.WriteLine(ex.Message);
 }
